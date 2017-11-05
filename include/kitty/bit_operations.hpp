ef28f9060dddb40200d69fd59b53e17adcb205cb
Mathias Soeken <mathias.soeken@epfl.ch>
2017-11-05 17:09:01 +0100

    Find bits in truth tables.

--- include/kitty/bit_operations.hpp
+++ include/kitty/bit_operations.hpp
@@ -117,4 +117,189 @@ void clear( static_truth_table<NumVars, true>& tt )
   tt._bits = 0;
 }
 /*! \endcond */
+
+/*! \cond PRIVATE */
+inline int64_t find_first_bit_in_word( uint64_t word )
+{
+  int64_t n = 0;
+  if ( word == 0 )
+  {
+    return -1;
+  }
+
+  if ( ( word & UINT64_C( 0x00000000FFFFFFFF ) ) == 0 )
+  {
+    n += 32;
+    word >>= 32;
+  }
+  if ( ( word & UINT64_C( 0x000000000000FFFF ) ) == 0 )
+  {
+    n += 16;
+    word >>= 16;
+  }
+  if ( ( word & UINT64_C( 0x00000000000000FF ) ) == 0 )
+  {
+    n += 8;
+    word >>= 8;
+  }
+  if ( ( word & UINT64_C( 0x000000000000000F ) ) == 0 )
+  {
+    n += 4;
+    word >>= 4;
+  }
+  if ( ( word & UINT64_C( 0x0000000000000003 ) ) == 0 )
+  {
+    n += 2;
+    word >>= 2;
+  }
+  if ( ( word & UINT64_C( 0x0000000000000001 ) ) == 0 )
+  {
+    n++;
+  }
+
+  return n;
+}
+
+inline int64_t find_last_bit_in_word( uint64_t word )
+{
+  int64_t n = 0;
+  if ( word == 0 )
+  {
+    return -1;
+  }
+
+  if ( ( word & UINT64_C( 0xFFFFFFFF00000000 ) ) == 0 )
+  {
+    n += 32;
+    word <<= 32;
+  }
+  if ( ( word & UINT64_C( 0xFFFF000000000000 ) ) == 0 )
+  {
+    n += 16;
+    word <<= 16;
+  }
+  if ( ( word & UINT64_C( 0xFF00000000000000 ) ) == 0 )
+  {
+    n += 8;
+    word <<= 8;
+  }
+  if ( ( word & UINT64_C( 0xF000000000000000 ) ) == 0 )
+  {
+    n += 4;
+    word <<= 4;
+  }
+  if ( ( word & UINT64_C( 0xC000000000000000 ) ) == 0 )
+  {
+    n += 2;
+    word <<= 2;
+  }
+  if ( ( word & UINT64_C( 0x8000000000000000 ) ) == 0 )
+  {
+    n++;
+  }
+
+  return 63 - n;
+}
+/*! \endcond */
+
+/*! \brief Finds least-significant one-bit
+
+  Returns -1, if truth table is constant 0.
+
+  \param tt Truth table
+*/
+template<typename TT>
+int64_t find_first_one_bit( const TT& tt )
+{
+  const auto it = std::find_if( tt.cbegin(), tt.cend(), []( auto word ) { return word != 0; } );
+
+  if ( it == tt.cend() )
+  {
+    return -1;
+  }
+  else
+  {
+    return 64 * std::distance( tt.cbegin(), it ) + find_first_bit_in_word( *it );
+  }
+}
+
+/*! \brief Finds most-significant one-bit
+
+  Returns -1, if truth table is constant 0.
+
+  \param tt Truth table
+*/
+template<typename TT>
+int64_t find_last_one_bit( const TT& tt )
+{
+  const auto it = std::find_if( tt.crbegin(), tt.crend(), []( auto word ) { return word != 0; } );
+
+  if ( it == tt.crend() )
+  {
+    return -1;
+  }
+  else
+  {
+    return 64 * ( std::distance( it, tt.crend() ) - 1 ) + find_last_bit_in_word( *it );
+  }
+}
+
+/*! \brief Finds least-significant bit difference
+
+  Returns -1, if truth tables are the same
+
+  \param first First truth table
+  \param second Second truth table
+*/
+template<typename TT>
+int64_t find_first_bit_difference( const TT& first, const TT& second )
+{
+  assert( first.num_vars() == second.num_vars() );
+
+  auto it = first.cbegin();
+  auto it2 = second.cbegin();
+  auto w = 0;
+
+  while ( it != first.cend() )
+  {
+    if ( *it ^ *it2 )
+    {
+      return 64 * w + find_first_bit_in_word( *it ^ *it2 );
+    }
+    ++it;
+    ++it2;
+    ++w;
+  }
+  return -1;
+}
+
+/*! \brief Finds most-significant bit difference
+
+  Returns -1, if truth tables are the same
+
+  \param first First truth table
+  \param second Second truth table
+*/
+template<typename TT>
+int64_t find_last_bit_difference( const TT& first, const TT& second )
+{
+  assert( first.num_vars() == second.num_vars() );
+
+  auto it = first.crbegin();
+  auto it2 = second.crbegin();
+  auto w = first.num_blocks() - 1;
+
+  while ( it != first.crend() )
+  {
+    if ( *it ^ *it2 )
+    {
+      return 64 * w + find_last_bit_in_word( *it ^ *it2 );
+    }
+    ++it;
+    ++it2;
+    --w;
+  }
+  return -1;
+}
+
 } // namespace kitty
