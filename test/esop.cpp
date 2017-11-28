287e5e546ea3dea0e6aa731202a5b326ff78b875
Mathias Soeken <mathias.soeken@epfl.ch>
2017-11-28 10:47:34 +0100

    Compute ESOP expressions.

---
+++ test/esop.cpp
@@ -0,0 +1,64 @@
+/* kitty: C++ truth table library
+ * Copyright (C) 2017  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cstdint>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <kitty/kitty.hpp>
+
+#include "utility.hpp"
+
+using namespace kitty;
+
+class EsopTest : public kitty::testing::Test
+{
+};
+
+TEST_F( EsopTest, esop_for_small )
+{
+  const auto tt = from_hex<3>( "e8" );
+
+  const auto cubes = esop_from_optimum_pkrm( tt );
+
+  auto tt_copy = tt.construct();
+  create_from_cubes( tt_copy, cubes, true );
+  EXPECT_EQ( tt, tt_copy );
+}
+
+TEST_F( EsopTest, random_esop )
+{
+  static_truth_table<10> tt;
+
+  for ( auto i = 0; i < 100; ++i )
+  {
+    create_random( tt );
+    const auto cubes = esop_from_optimum_pkrm( tt );
+    auto tt_copy = tt.construct();
+    create_from_cubes( tt_copy, cubes, true );
+    EXPECT_EQ( tt, tt_copy );
+  }
+}
