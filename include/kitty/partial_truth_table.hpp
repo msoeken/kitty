bb041e7bec42913769240255198054fa27bd9389
Sonia <lee30sonia@gmail.com>
2020-12-14 19:18:45 +0100

    small fixes

--- include/kitty/partial_truth_table.hpp
+++ include/kitty/partial_truth_table.hpp
@@ -1,5 +1,5 @@
 /* kitty: C++ truth table library
- * Copyright (C) 2017-2019  EPFL
+ * Copyright (C) 2017-2020  EPFL
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -32,14 +32,15 @@
 
 #pragma once
 
+#include "detail/constants.hpp"
+#include "traits.hpp"
+
 #include <cstdint>
 #include <type_traits>
 #include <vector>
 #include <iostream>
 #include <iomanip>
-
-#include "detail/constants.hpp"
-#include "traits.hpp"
+#include <cassert>
 
 namespace kitty
 {
