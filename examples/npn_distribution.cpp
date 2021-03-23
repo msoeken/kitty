28eff42d8d3557bb0476d4c11fb5f82004996505
Pulkit Agrawal <pulkitag22@gmail.com>
2021-03-24 02:29:44 +0530

    Update npn_distribution.cpp

--- examples/npn_distribution.cpp
+++ examples/npn_distribution.cpp
@@ -9,11 +9,19 @@ int main () {
 	auto class_sd = kitty::calculate_sd_represtative_class(4u);
 	auto class_npn = kitty::calculate_npn_represtative_class(4u);
 	kitty::dynamic_truth_table array_npn[222];
+	kitty::dynamic_truth_table array_sd[83];
 
 	itr = class_npn.begin();
 	int i = 0;
 	for (itr=class_npn.begin(); itr!=class_npn.end(); itr++){
 		array_npn[i] = *itr;
+		i++;
+	}
+
+	itr = class_sd.begin();
+	i = 0;
+	for (itr=class_sd.begin(); itr!=class_sd.end(); itr++){
+		array_sd[i] = *itr;
 		i++;
 	}
 	
@@ -24,10 +32,16 @@ int main () {
 		auto temp = kitty::exact_sd_canonization(array_npn[i]);
 		for (j=0; j<83; j++){
 			distribution[j] = 0;
+			if (kitty::equal(temp, array_sd[j])){
+				distribution[j]+=1;
+				break;
+			}
 		}
-		if (class_sd.find(temp)!=class_sd.end()){
-			std::cout<<"Oh Yeah";
-		}
+	}
+	
+	i=0;
+	for (i=0; i<83; i++){
+		std::cout<<i<<std::endl;
 	}
 	return 0;
 }
