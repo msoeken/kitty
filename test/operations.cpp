9b8b38cdd6df5979893425d23957c7c47471cbfd
Sonia <lee30sonia@gmail.com>
2021-04-25 23:08:09 +0200

    tests & docs

--- test/operations.cpp
+++ test/operations.cpp
@@ -818,3 +818,11 @@ TEST_F( OperationsTest, shift_with_mask )
   EXPECT_EQ( shift_with_mask( from_hex<6>( "e8e8e8e8e8e8e8e8" ), 0b111000 ), from_hex<6>( "ffffff00ff000000" ) );
   EXPECT_EQ( shift_with_mask( from_hex<6>( "7778777877787778" ), 0b101101 ), from_hex<6>( "5f5f5f5f5fa05fa0" ) );
 }
+
+TEST_F( OperationsTest, intersection_is_empty )
+{
+  EXPECT_TRUE( intersection_is_empty( from_hex<3>( "a2" ), from_hex<3>( "5c" ) ) );
+  EXPECT_TRUE( intersection_is_empty( from_hex( 3, "a2" ), from_hex( 3, "5c" ) ) );
+  EXPECT_TRUE( !intersection_is_empty( from_hex<3>( "f0" ), from_hex<3>( "5c" ) ) );
+  EXPECT_TRUE( !intersection_is_empty( from_hex( 3, "f0" ), from_hex( 3, "5c" ) ) );
+}
